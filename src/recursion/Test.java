0f5f43550c4436c73f1b81341e13d372a2bff37c
孟木子 <18351923886@163.com>
2019-07-27 12:31:19 +0800

    迷宫-》回溯法

---
+++ src/recursion/Test.java
@@ -0,0 +1,30 @@
+package recursion;
+
+/**
+ * @author : mengmuzi
+ * create at:  2019-07-27  10:55
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        test(4);
+        int result = factorial(4);
+        System.out.println(result);
+    }
+
+    public static void test(int n){
+        if(n > 2){
+            test(n - 1);
+
+        }
+        System.out.println("n =" + n);
+    }
+
+    public static int factorial(int n){
+        if(n == 1){
+            return 1;
+        }else{
+            return factorial(n - 1) * n;
+        }
+    }
+}
