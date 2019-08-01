9bbd1d21c02d89c3c6a247f8cc27993aba8fc23e
孟木子 <18351923886@163.com>
2019-08-01 20:45:34 +0800

    二分查找（使用二分查找找出全部的待查找值

--- src/java_coding_han_DataStructures/search/BinarySearch.java
+++ src/java_coding_han_DataStructures/search/BinarySearch.java
@@ -1,5 +1,8 @@
 package java_coding_han_DataStructures.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author : mengmuzi
  * create at:  2019-08-01  17:56
@@ -7,9 +10,12 @@ package java_coding_han_DataStructures.search;
  */
 public class BinarySearch {
     public static void main(String[] args) {
-        int arr[] = {1, 8, 10, 89, 1000, 1000, 1234};
-        int resultIndex = binarySearch(arr,0,arr.length,-10);
+        int arr[] = {1, 8, 10, 89, 1000, 1000, 1000, 1000, 1234};
+        int resultIndex = binarySearch(arr, 0, arr.length, -10);
         System.out.println(resultIndex);
+
+        List<Integer> list = binarySearch02(arr, 0, arr.length, 1000);
+        System.out.println(list);
 
     }
 
@@ -39,5 +45,64 @@ public class BinarySearch {
             return mid;
         }
     }
+
+
+    //完成一个课后思考题:
+
+    /**
+     * 课后思考题: {1,8, 10, 89, 1000, 1000，1234} 当一个有序数组中，
+     * 有多个相同的数值时，如何将所有的数值都查找到，比如这里的 1000思路分析
+     * <p>
+     * 1. 在找到 mid 索引值，不要马上返回
+     * 2. 向 mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合 ArrayList
+     * 3. 向 mid 索引值的右边扫描，将所有满足 1000， 的元素的下标，加入到集合 ArrayList
+     * 4. 将 Arraylist 返回
+     */
+
+    public static List<Integer> binarySearch02(int[] arr, int left, int right, int findVal) {
+        // 当 left > right 时，说明递归整个数组，但是没有找到
+        if (left > right) {
+            return new ArrayList<Integer>();
+        }
+
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+        if (midVal < findVal) {//向右递归
+            return binarySearch02(arr, mid + 1, right, findVal);
+
+        } else if (midVal > findVal) {//向左递归
+            return binarySearch02(arr, left, mid - 1, findVal);
+        } else {
+            // * 思路分析
+            // * 1. 在找到 mid 索引值，不要马上返回
+            // * 2. 向 mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合 ArrayList
+            // * 3. 向 mid 索引值的右边扫描，将所有满足 1000， 的元素的下标，加入到集合 ArrayList
+            // * 4. 将 Arraylist 返回
+            List<Integer> resIndexList = new ArrayList<>();
+
+            //向 mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合 ArrayList
+            int temp = mid - 1;
+            while (true) {
+                if (temp < 0 || arr[temp] != findVal) {
+                    break;
+                }
+                resIndexList.add(temp);
+                temp--;
+            }
+
+            resIndexList.add(mid);//原来的别忘了
+
+            temp = mid + 1;
+            while (true) {
+                if (temp > arr.length - 1 || arr[temp] != findVal) {
+                    break;
+                }
+                resIndexList.add(temp);
+                temp++;
+            }
+            return resIndexList;
+        }
+    }
+
 
 }
