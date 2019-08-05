308baa555e5faab29cb4262220dcf6f3b50b15ac
孟木子 <18351923886@163.com>
2019-08-05 12:04:26 +0800

    贪心算法

--- src/swordoffer/DuplicateNumber.java
+++ src/swordoffer/DuplicateNumber.java
@@ -16,12 +16,12 @@ import java.util.Set;
  */
 public class DuplicateNumber {
     public boolean duplicate(int numbers[], int length, int[] duplication) {
-        if(numbers == null || numbers.length < 2){
+        if (numbers == null || numbers.length < 2) {
             return false;
         }
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < numbers.length ; i++) {
-            if(!set.add(numbers[i])){
+        for (int i = 0; i < numbers.length; i++) {
+            if (!set.add(numbers[i])) {
                 duplication[0] = numbers[i];
                 return true;
             }
@@ -32,17 +32,17 @@ public class DuplicateNumber {
 
 
     public boolean duplicate02(int numbers[], int length, int[] duplication) {
-        if(numbers == null || numbers.length < 2){
+        if (numbers == null || numbers.length < 2) {
             return false;
         }
         for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] != i){
+            if (numbers[i] != i) {
                 int val = numbers[numbers[i]];
-                if(val == numbers[i]){
+                if (val == numbers[i]) {
                     duplication[0] = numbers[i];
                     return true;
                 }
-                swap(numbers,i,numbers[i]);
+                swap(numbers, i, numbers[i]);
             }
         }
         return false;
@@ -50,8 +50,41 @@ public class DuplicateNumber {
 
     private void swap(int[] numbers, int i, int j) {
         int temp = numbers[i];
-        numbers[i] = numbers[j] ;
+        numbers[i] = numbers[j];
         numbers[j] = temp;
+    }
+
+
+    public int duplicateInArray(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return -1;
+        }
+        int start = 1;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + ((end - start) >> 1);
+            int count = getCount(nums, start, mid);
+            if (start == end) {
+                if (count > 1) {
+                    return start;
+                }
+            } else if (count < mid - start + 1) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int getCount(int[] nums, int start, int mid) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (start <= nums[i] && nums[i] <= mid) {
+                count++;
+            }
+        }
+        return count;
     }
 
 
